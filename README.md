11c6423d2632488687a247b19c8a572cfa23c05d
Aidan Hijazi-Klop <aidanlhk@gmail.com>
2022-03-28 15:41:36 -0400

    added express?

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cs4750-crudrepo
+
+npm i express body-parser mysql2
+npm i -D nodemon
---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      // get Google Cloud info here....
+      host: "",
+      user: "",
+      password: "",
+      database: "",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
